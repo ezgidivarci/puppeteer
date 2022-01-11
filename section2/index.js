73f39826cd0dff4748456079fd7e7ca893cd639f
Ezgi Dıvarcı <ezgi.divarci@modanisa.com>
2022-01-11 11:19:53 +0300

    add section2

--- section2/index.js
+++ section2/index.js
@@ -1,10 +1,12 @@
+//listing brand names
+
 const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch(); // {headless : false} tarayıcıyı görmemizi sağlıyor
   const page = await browser.newPage();
   await page.goto('https://www.modanisa.com/elbise.list');
-  await page.screenshot({ path: 'example.png' });
+  //await page.screenshot({ path: 'example.png' });
 
   const result = await page.evaluate (() => {
     let brandNames = document.querySelectorAll(".brand")
@@ -16,9 +18,3 @@ const puppeteer = require('puppeteer');
 
   await browser.close();
 })();
-
-/*
-
-
-
-*/
